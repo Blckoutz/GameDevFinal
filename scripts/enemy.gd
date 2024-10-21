f3d359ff54b214d5246878da8a640fbad4bec05b
Blckoutz <ibrahimm3@newpaltz.edu>
2024-10-21 16:43:08 -0400

    2 hrs
    minor updates to fix pathfinding (needs extra work), fixing some things with enemy script and giving a way to detect the player from some distance away to start tracking the player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,11 +4,44 @@ extends CharacterBody2D
 @export var pathfinding: PathFinding
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.play("idle")  # Start with idle animation
+
+
+func _process(delta: float) -> void:
+	_on_a_idetection_area_entered($AIdetection)
+	_roam()
+func _on_a_idetection_area_entered(area: Area2D) -> void:
+	_track()
 
 func _track():
-	velocity = pathfinding.direction *200
+	# Set velocity using the pathfinding direction
+	velocity = pathfinding.direction * 200
 	move_and_slide()
+
+	# Play animation based on direction
+	update_animation(pathfinding.direction)
+
+
 func _roam():
+	# Set velocity using the wander direction
 	velocity = wander_direction.direction * 200
 	move_and_slide()
+
+	# Play animation based on direction
+	update_animation(wander_direction.direction)
+
+
+func update_animation(direction: Vector2):
+	# Determine which animation to play based on direction
+	if direction.x > 0:
+		$AnimatedSprite2D.flip_h = false  # Moving right, do not flip
+		$AnimatedSprite2D.play("walkSide")
+	elif direction.x < 0:
+		$AnimatedSprite2D.flip_h = true   # Moving left, flip the sprite
+		$AnimatedSprite2D.play("walkSide")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("walkF")   # Moving down
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("walkB")   # Moving up
+	else:
+		$AnimatedSprite2D.play("idle")    # Not moving, play idle
--- scripts/pathfinding.gd
+++ scripts/pathfinding.gd
@@ -3,7 +3,7 @@ class_name PathFinding
  #Code by 16BitDev on YouTube, https://www.youtube.com/watch?v=0X1ReGZJmXU
  
 @onready var navigation_agent_2d = $NavigationAgent2D
-@onready var target : CharacterBody2D = owner.get_parent().find_child("Player")
+@onready var target : CharacterBody2D = owner.get_parent().find_child("tempPlayer")
 var direction = Vector2.ZERO
  
  
